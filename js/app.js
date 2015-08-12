29943214286f70cce45a915ba1034853bc87c0b0
Zhukaff <ajaja49@gmail.com>
2015-08-12 16:50:18 +0400

    Finished overflow, and gallery style

--- js/app.js
+++ js/app.js
@@ -29,30 +29,44 @@ var app = {
 	 */
 	main: function ()
 	{
-		var image_count = document.getElementById("gallery").childElementCount - 2;
-		this.gallery = new Gallery({
-			id: "gallery",
-			slide_count: image_count,
-			vertical: false
-		});
-		this.gallery.autoScroll(10000);
+		var isIE = false;
+		if (navigator.userAgent.indexOf("MSIE 8.0") > -1)  // I think people dont use IE7, arent they 
+			isIE = true;
 
-		var comment_count = document.getElementById("comments").childElementCount;
-		this.comments = new Gallery({
-			id: "comments",
-			slide_count: comment_count,
-			vertical: true
-		});
-		this.comments.autoScroll(15000);
+		if (!isIE)
+		{
+			var image_count = document.getElementById("gallery").childElementCount - 2;
+			this.gallery = new Gallery({
+				id: "gallery",
+				slide_count: image_count,
+				vertical: false
+			});
+			this.gallery.autoScroll(10000);
 
-		var footer = document.getElementsByTagName("footer")[0];
-		lib.addCrossBrowserEvent(footer, "mouseenter", this.footer_scroll);
+			var comment_count = document.getElementById("comments").childElementCount;
+			this.comments = new Gallery({
+				id: "comments",
+				slide_count: comment_count,
+				vertical: true
+			});
+			this.comments.autoScroll(15000);
 
-		var btn_left = document.getElementById("left");
-		var btn_right = document.getElementById("right");
-		lib.addCrossBrowserEvent(btn_left, "click", this.gallery_left);
-		lib.addCrossBrowserEvent(btn_right, "click", this.gallery_right);
-		
+			var footer = document.getElementsByTagName("footer")[0];
+			lib.addCrossBrowserEvent(footer, "mouseenter", this.footer_scroll);
+
+			var btn_left = document.getElementById("left");
+			var btn_right = document.getElementById("right");
+			lib.addCrossBrowserEvent(btn_left, "click", this.gallery_left);
+			lib.addCrossBrowserEvent(btn_right, "click", this.gallery_right);
+		}
+		else
+		{
+			var gallery = document.getElementById("gallery");
+			var comments = document.getElementById("comments");
+			gallery.setAttribute("style", "overflow-x: scroll;");
+			comments.setAttribute("style", "overflow-y: scroll;");
+		}
+
 		var links = document.getElementById("gallery").getElementsByTagName("a");
 		for (var i = 0; i < links.length; i++)
 		{

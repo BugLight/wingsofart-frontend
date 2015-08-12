50328ebc0592daee6e7808c57bf8c988284b1007
Radik <ajaja49@gmail.com>
2015-08-12 23:07:23 +0300

    Animation class template

---
+++ js/animation.js
@@ -0,0 +1,12 @@
+/**
+ * Created by BugLight on 12.08.2015.
+ * This class realises animation effect on web pages.
+ * @class Animation
+ * @author BugLight
+ *  @param {Object} config Init configuration
+ *      @param
+ */
+Animation = function (config)
+{
+
+};
